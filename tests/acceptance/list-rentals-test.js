27d29970d364fc4f3f40f14e9d7de88615b33fc7
vsavard <vero@nuagesdencre.com>
2019-02-06 10:36:51 +0000

    updated test to use mirage

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,10 +1,12 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
-
+  setupMirage(hooks);
   test('visiting /', async function(assert) {
     await visit('/');
 
