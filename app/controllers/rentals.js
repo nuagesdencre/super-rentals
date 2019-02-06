54770ef924897c98fc31f8c33f9b3843206072d9
vsavard <vero@nuagesdencre.com>
2019-02-06 14:04:25 +0000

    reviewing filter tweaking

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -1,10 +1,5 @@
 import Controller from '@ember/controller';
 
-// Controllers contain actions and properties available to the template of its corresponding route.
-//   In our case we want to generate a controller called rentals.
-//   Ember will know that a controller with the name of rentals will apply to the route with the same name.
-
-
 export default Controller.extend({
   actions: {
     filterByCity(param) {
