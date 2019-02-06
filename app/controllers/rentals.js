459b09591783c5a89916fead0afff77bdabbf8ab
vsavard <vero@nuagesdencre.com>
2019-02-06 10:59:38 +0000

    setting up controller and adding comments

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -1,4 +1,18 @@
 import Controller from '@ember/controller';
 
+// Controllers contain actions and properties available to the template of its corresponding route.
+//   In our case we want to generate a controller called rentals.
+//   Ember will know that a controller with the name of rentals will apply to the route with the same name.
+
+
 export default Controller.extend({
+  actions: {
+    filterByCity(param) {
+      if (param !== '') {
+        return this.store.query('rental', {city: param});
+      } else {
+        return this.store.findAll('rental');
+      }
+    }
+  }
 });
